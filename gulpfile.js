bffea1c142df73e740ec84a76905de058b888b71
Paulo Santos <paulo.santos@ifnet-for.ifactory.com.br>
2017-12-01 19:01:19 -0300

    Adding SCSS preprocessor

--- gulpfile.js
+++ gulpfile.js
@@ -7,23 +7,39 @@ var  gulp = require('gulp')
 	,usemin = require('gulp-usemin')
     ,cssmin = require('gulp-cssmin')
 	,path = require('gulp-path')
+	,sass = require('gulp-sass')
 	,runSequence = require('run-sequence')
+	,sourcemaps = require('gulp-sourcemaps')
+	,autoprefixer = require('gulp-autoprefixer')
 	,clean = require('gulp-clean');
+	
+var input = 'src/p8queiroz/scss/**/*.scss';
+var output = 'src/p8queiroz/css';
+var sassOptions = {
+					  errLogToConsole: true,
+					  outputStyle: 'expanded'
+					};
+
+var autoprefixerOptions = {
+  browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
+};
 
 
-gulp.task('default', function(callback) {
-    runSequence('copylocal', ['copyremote'], callback);
-});
+	gulp.task('default', function(callback) {
+		runSequence('copylocal', ['copyremote'], callback);
+	});
 
 
 ////
 	gulp.task('copylocal', ['cleanlocal'], function() {
-		return gulp.src('src/p8queiroz/**/*')
+		return gulp
+			.src('src/p8queiroz/**/*')
 			.pipe(gulp.dest('dist/p8queiroz'));
 	});
 
-	gulp.task('cleanlocal', ['buildimg'], function() {
-		return gulp.src('dist')
+	gulp.task('cleanlocal', ['buildimg', 'sass', 'watchsass'], function() {
+		return gulp
+			.src('dist')
 			.pipe(clean({force: true}));
 			
 	});
@@ -31,7 +47,8 @@ gulp.task('default', function(callback) {
 
 ////
 	gulp.task('buildimg', function() {
-		return gulp.src('src/p8queiroz/images/**/*')
+		return gulp
+			.src('src/p8queiroz/images/**/*')
 			.pipe(imagemin())
 			.pipe(gulp.dest('src/p8queiroz/images'));
 	});
@@ -39,17 +56,39 @@ gulp.task('default', function(callback) {
 
 ////
 	gulp.task('copyremote', ['cleanremote'], function() {
-		return gulp.src('dist/p8queiroz/**/*')
+		return gulp
+			.src('dist/p8queiroz/**/*')
 			.pipe(gulp.dest('C:/Program Files/VertrigoServ/www/p8queiroz/wp-content/themes/p8queiroz'));
 	});
 
 	gulp.task('cleanremote', function() {
-		return gulp.src('C:/Program Files/VertrigoServ/www/p8queiroz/wp-content/themes/p8queiroz')
+		return gulp
+			.src('C:/Program Files/VertrigoServ/www/p8queiroz/wp-content/themes/p8queiroz')
 			.pipe(clean({force: true}));
 			
 	});
 ////--
 
 
+////
+	gulp.task('sass', function () {
+	  return gulp
+				.src(input)
+				//.pipe(sourcemaps.init())
+				.pipe(sass(sassOptions).on('error', sass.logError))
+				///.pipe(sourcemaps.write())
+				.pipe(autoprefixer(autoprefixerOptions))
+				.pipe(gulp.dest(output));
+	});
 
 
+	
+	gulp.task('watchsass', function() {
+	  return gulp
+		 	.watch(input, ['sass'])
+			.on('change', function(event) {
+			 console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+		});
+	});
+
+////--
